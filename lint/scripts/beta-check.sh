b86880a00d8ecbb600c95ec36336e3d89988f498
Omar Jaroudi <ojaroudi@turo.com>
2023-02-24 21:44:34 +0000

    refactor(beta-check): allow multiple prefixes

--- lint/scripts/beta-check.sh
+++ lint/scripts/beta-check.sh
@@ -1,6 +1,7 @@
 set -e
 
-if egrep "$1.*\-beta\-" package.json; then
+delimited_prefixes=$(sed 's/ *, */|/g' <<< $1)
+if egrep "($delimited_prefixes).*\-beta\-" package.json; then
     echo "ERROR: should not consume a beta version of an internal dependency. Make sure to revert/update to use a proper release."
     exit 1
 fi
