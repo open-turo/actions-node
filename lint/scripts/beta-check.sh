9354a0785654f518b349735f285a3bdbfbffee95
Omar Jaroudi <ojaroudi@turo.com>
2023-02-28 18:11:18 +0000

    fix(beta-check): embed bash script in action
    we can't access the script file from the consumer running this action

--- lint/scripts/beta-check.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-delimited_prefixes=$(sed 's/ *, */|/g' <<< $1)
-if egrep "($delimited_prefixes).*\-beta\-" package.json; then
-    echo "ERROR: should not consume a beta version of an internal dependency. Make sure to revert/update to use a proper release."
-    exit 1
-fi
